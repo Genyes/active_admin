cb9873b115f7beae7f7d6b78b1a1fe74f2cd63a2
Brennan Shacklett <bpshacklett@gmail.com>
2014-08-13 11:52:49 -0700

    switch site title to tabbed nav

--- lib/active_admin/view_factory.rb
+++ lib/active_admin/view_factory.rb
@@ -6,7 +6,7 @@ module ActiveAdmin
     # Register Helper Renderers
     register  global_navigation:   ActiveAdmin::Views::TabbedNavigation,
               utility_navigation:  ActiveAdmin::Views::TabbedNavigation,
-              site_title:          ActiveAdmin::Views::SiteTitle,
+              site_title:          ActiveAdmin::Views::TabbedNavigation,
               action_items:        ActiveAdmin::Views::ActionItems,
               title_bar:           ActiveAdmin::Views::TitleBar,
               header:              ActiveAdmin::Views::Header,
